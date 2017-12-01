8504b153049e1d420ac4a1355a5e382059cb0e00
SRAUFactory <srau.factory@gmail.com>
2017-12-01 22:17:50 +0900

    WIP

--- matrix.go
+++ matrix.go
@@ -12,93 +12,47 @@ type MatchSchedule struct {
 	Max   int
 }
 
-// 対戦順を出力
-func printMatchSchdule(matches *MatchSchedule) {
-	for iy := 0; iy < matches.Num; iy++ {
-		for ix := 0; ix < matches.Num; ix++ {
-			if ix == iy {
-				fmt.Print("-")
-			} else {
-				fmt.Print(matches.Match[ix][iy])
-			}
-		}
-		fmt.Println()
+func CheckTargetValue(num int, target []int) []int {
+	if target[1] > num {
+		target[1] = target[1] % num
+	} else if target[0] == target[1] {
+		target[1]++
+	} else if target[0] > target[1] {
+		tmp := target[1]
+		target[1] = target[0]
+		target[0] = tmp
+	} else {
+		return target
 	}
-	j := 1
-	for i := 1; i <= matches.Max; i++ {
-		for iy := 0; iy < matches.Num; iy++ {
-			for ix := iy + 1; ix < matches.Num; ix++ {
-				if matches.Match[ix][iy] != i {
-					continue
-				}
-				fmt.Printf("%d: %d - %d\n", j, iy+1, ix+1)
-				j++
-			}
-		}
-	}
-}
-
-// 設定済みの値を取得する
-func getSetValueList(matches *MatchSchedule, ix int, iy int) []int {
-	values := []int{}
-	for i := 0; i < ix; i++ {
-		values = append(values, matches.Match[i][iy])
-	}
-	for i := 0; i < iy; i++ {
-		values = append(values, matches.Match[ix][i])
-	}
-	return values
-}
-
-// 抽出した値の中に含まれていない値の最小値を算出
-func getMinmumValue(matches *MatchSchedule, values []int, min int) int {
-	for j := min; j <= matches.Max; j++ {
-		isFound := false
-		for i := 0; i < len(values); i++ {
-			if j == values[i] {
-				isFound = true
-				break
-			}
-		}
-		if !isFound {
-			min = j
-			break
-		} else if j == matches.Max {
-			j = 0
-		}
-		continue
-	}
-	return min
+	return CheckTargetValue(num, target)
 }
 
 // 対戦順を生成
 func createMatchSchdule(num int) *MatchSchedule {
-	// 対戦表を作るチーム数は偶数になるように設定
-	// 奇数の場合は+1する
-	n := num/2 + num%2
-	n2 := n * 2
-
-	// 1チームあたりの最大試合数
-	matches := &MatchSchedule{Max: 2*n - 1, Num: num}
-	matches.Match = make([][]int, n2)
-	for i := 0; i < n2; i++ {
-		matches.Match[i] = make([]int, n2)
-	}
-
 	// 対戦順生成
-	for iy := 0; iy < n2; iy++ {
-		for ix := iy + 1; ix < n2; ix++ {
-			if ix == iy {
-				continue
+	matches := &MatchSchedule{Max: num * (num - 1) / 2, Num: num, Match: [][]int{}}
+	turn := 1
+	current := 1
+	for i := 0; i < matches.Max; i++ {
+		target := CheckTargetValue(matches.Num, []int{current, current + turn})
+
+		for j := 0; j < len(matches.Match); j++ {
+			if matches.Match[j][0] == target[0] && matches.Match[j][1] == target[1] {
+				break
 			}
+			target[1]++
+			target = CheckTargetValue(matches.Num, target)
+		}
 
-			// 対象行/列で設定済みの値をまとめて抽出
-			values := getSetValueList(matches, ix, iy)
-
-			// 抽出した値の中に含まれていない値の最小値を算出
-			min := ix
-			matches.Match[ix][iy] = getMinmumValue(matches, values, min)
-			matches.Match[iy][ix] = matches.Match[ix][iy]
+		matches.Match = append(matches.Match, target)
+		fmt.Println(matches.Match)
+		current++
+		if turn == 1 {
+			current++
+		}
+		if current > matches.Num {
+			current = 1
+			turn++
 		}
 	}
 	return matches
@@ -112,6 +66,5 @@ func main() {
 
 	// 対戦表作成
 	matches := createMatchSchdule(num)
-	// 対戦順出力
-	printMatchSchdule(matches)
+	fmt.Println(matches.Match)
 }
