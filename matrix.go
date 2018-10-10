1fd72e029642b3ff2ccaf07cb008be58b71984c6
SRAUFactory <srau.factory@gmail.com>
2018-10-10 18:57:29 +0900

    feat [WIP]add debug code

--- matrix.go
+++ matrix.go
@@ -7,24 +7,29 @@ import (
 
 func CreateMatchList(num int) [][]int {
 	matchNum := num * (num - 1) / 2
+	turnMatchNum := num/2 + num%2
+	fmt.Printf("CreateMatchList %d, %d, %d\n", num, matchNum, turnMatchNum)
 	matchList := [][]int{}
 
 	current := 1
 	turn := 1
-	for i := 0; i < matchNum; i++ {
+	for i := 1; i <= matchNum; i++ {
+		fmt.Printf("start %d, %d, %d\n", i, current, turn)
 		target := []int{current, current + turn}
 		if target[1] > num {
 			target[1] = target[1] % num
 		}
+		fmt.Println(target)
 
 		matchList = append(matchList, target)
 		current++
-		if current >= num || target[1] > num {
+		if i%turnMatchNum == 0 {
 			current = 1
 			turn++
 		} else if turn == 1 {
 			current = target[1] + turn
 		}
+		fmt.Printf("end %d, %d, %d\n", i, current, turn)
 	}
 	return matchList
 }
