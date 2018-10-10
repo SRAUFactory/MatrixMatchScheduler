e2e74c7718c3214369f839473967cfe1c3bb9559
SRAUFactory <srau.factory@gmail.com>
2018-10-10 19:23:12 +0900

    feat: [WIP]add function of `IsExistList`

--- matrix.go
+++ matrix.go
@@ -5,6 +5,19 @@ import (
 	"fmt"
 )
 
+func IsExistList(matchList [][]int, target []int) bool {
+	for i := 0; i < len(matchList)-1; i++ {
+		currentMatch := matchList[i]
+		if currentMatch[0] == target[0] && currentMatch[1] == target[1] {
+			return true
+		}
+		if currentMatch[0] == target[1] && currentMatch[1] == target[0] {
+			return true
+		}
+	}
+	return false
+}
+
 func CreateMatchList(num int) [][]int {
 	matchNum := num * (num - 1) / 2
 	turnMatchNum := num/2 + num%2
@@ -18,6 +31,15 @@ func CreateMatchList(num int) [][]int {
 		target := []int{current, current + turn}
 		if target[1] > num {
 			target[1] = target[1] % num
+		}
+
+		for IsExistList(matchList, target) {
+			target[1]++
+			if target[1] > num {
+				target[1] = target[1] % num
+			} else if target[0] == target[1] {
+				break
+			}
 		}
 		fmt.Println(target)
 
