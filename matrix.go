97d8a4aedbc6176947ed5a07e6d819628a70f718
SRAUFactory <srau.factory@gmail.com>
2018-10-16 17:50:12 +0900

    feat: 対戦カード作成処理追加

--- matrix.go
+++ matrix.go
@@ -5,11 +5,25 @@ import (
 	"fmt"
 )
 
+func createMatchCards(num int) [][]int {
+	matchCards := [][]int{}
+	for iy := 1; iy <= num; iy++ {
+		for ix := iy + 1; ix <= num; ix++ {
+			current := []int{iy, ix}
+			matchCards = append(matchCards, current)
+		}
+	}
+
+	return matchCards
+}
+
 func main() {
 	var num int
 	// 引数からチーム数設定
 	flag.IntVar(&num, "num", 0, "int flag")
 	flag.Parse()
-
 	fmt.Printf("チーム数: %d\n", num)
+
+	matchCards := createMatchCards(num)
+	fmt.Println(matchCards)
 }
