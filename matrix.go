be0396328039e3dccbee91253a99d3914f7ed8e6
SRAUFactory <srau.factory@gmail.com>
2018-10-16 17:44:18 +0900

    feat: 不要なコードを一旦削除

--- matrix.go
+++ matrix.go
@@ -5,105 +5,11 @@ import (
 	"fmt"
 )
 
-// 試合スケジュール
-type MatchSchedule struct {
-	Match [][]int
-	Num   int
-	Max   int
-}
-
-// 対戦順を出力
-func printMatchSchdule(matches *MatchSchedule) {
-	for iy := 0; iy < matches.Num; iy++ {
-		for ix := 0; ix < matches.Num; ix++ {
-			if ix == iy {
-				fmt.Print("-")
-			} else {
-				fmt.Print(matches.Match[ix][iy])
-			}
-		}
-		fmt.Println()
-	}
-	j := 1
-	for i := 1; i <= matches.Max; i++ {
-		for iy := 0; iy < matches.Num; iy++ {
-			for ix := iy + 1; ix < matches.Num; ix++ {
-				if matches.Match[ix][iy] != i {
-					continue
-				}
-				fmt.Printf("%d: %d - %d\n", j, iy+1, ix+1)
-				j++
-			}
-		}
-	}
-}
-
-// 設定済みの値を取得する
-func getSetValueList(matches *MatchSchedule, ix int, iy int) []int {
-	values := matches.Match[ix]
-	for i := 0; i < ix; i++ {
-		values = append(values, matches.Match[i][iy])
-	}
-	return values
-}
-
-// 抽出した値の中に含まれていない値の最小値を算出
-func getMinmumValue(matches *MatchSchedule, values []int, min int) int {
-	for j := min; j <= matches.Max; j++ {
-		isFound := false
-		for i := 0; i < len(values); i++ {
-			if j == values[i] {
-				isFound = true
-				break
-			}
-		}
-		if !isFound {
-			min = j
-			break
-		} else if j == matches.Max {
-			j = 0
-		}
-		continue
-	}
-	return min
-}
-
-// 対戦順を生成
-func createMatchSchdule(num int) *MatchSchedule {
-	// 対戦表を作るチーム数は偶数になるように設定
-	// 奇数の場合は+1する
-	n := num/2 + num%2
-	n2 := n * 2
-
-	// 1チームあたりの最大試合数
-	matches := &MatchSchedule{Max: 2*n - 1, Num: num}
-	matches.Match = make([][]int, n2)
-	for i := 0; i < n2; i++ {
-		matches.Match[i] = make([]int, n2)
-	}
-
-	// 対戦順生成
-	for iy := 0; iy < n2; iy++ {
-		for ix := iy + 1; ix < n2; ix++ {
-			// 対象行/列で設定済みの値をまとめて抽出
-			values := getSetValueList(matches, ix, iy)
-
-			// 抽出した値の中に含まれていない値の最小値を算出
-			matches.Match[ix][iy] = getMinmumValue(matches, values, ix)
-			matches.Match[iy][ix] = matches.Match[ix][iy]
-		}
-	}
-	return matches
-}
-
 func main() {
 	var num int
 	// 引数からチーム数設定
 	flag.IntVar(&num, "num", 0, "int flag")
 	flag.Parse()
 
-	// 対戦表作成
-	matches := createMatchSchdule(num)
-	// 対戦順出力
-	printMatchSchdule(matches)
+	fmt.Printf("チーム数: %d\n", num)
 }
